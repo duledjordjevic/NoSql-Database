e833544f6f9838748e154f625e6fd514be8ed2cb
duledjordjevic <djordjevic.sv1.2021@uns.ac.rs>
2023-01-28 19:23:53 +0100

    adding print function

--- Structures/Btree/btree.go
+++ Structures/Btree/btree.go
@@ -12,8 +12,8 @@ type Structure interface {
 
 type Node struct {
 	parent   *Node
-	keys     []*Record.Record
-	children []*Node
+	Keys     []*Record.Record
+	Children []*Node
 	isLeaf   bool
 	n        int
 	T        int
@@ -27,28 +27,28 @@ func createNode(T int) *Node {
 		T:      T,
 	}
 
-	node.keys = make([]*Record.Record, node.T-1)
-	node.children = make([]*Node, node.T)
+	node.Keys = make([]*Record.Record, node.T-1)
+	node.Children = make([]*Node, node.T)
 
 	for i := 0; i < node.T; i++ {
 		if i != node.T-1 {
-			node.keys[i] = nil
+			node.Keys[i] = nil
 		}
-		node.children[i] = nil
+		node.Children[i] = nil
 	}
 	return node
 }
 
 type BTree struct {
-	root *Node
+	Root *Node
 	T    int
 	n    int
 }
 
-func createBTree(T int) *BTree {
+func CreateBTree(T int) *BTree {
 	bTree := &BTree{
 		n:    0,
-		root: nil,
+		Root: nil,
 		T:    T,
 	}
 
@@ -56,18 +56,18 @@ func createBTree(T int) *BTree {
 }
 
 func (bTree *BTree) Search(key string) *Node {
-	if bTree.root == nil {
+	if bTree.Root == nil {
 		return nil
 	}
-	currentNode := bTree.root
+	currentNode := bTree.Root
 	tf := true
 	for tf {
 		indexChild := 0
 		for i := 0; i < currentNode.n; i++ {
-			if currentNode.keys[i].GetKey() == key {
+			if currentNode.Keys[i].GetKey() == key {
 				return currentNode
 			}
-			if key < currentNode.keys[i].GetKey() {
+			if key < currentNode.Keys[i].GetKey() {
 				break
 			}
 			indexChild++
@@ -75,7 +75,7 @@ func (bTree *BTree) Search(key string) *Node {
 		if currentNode.isLeaf {
 			break
 		}
-		currentNode = currentNode.children[indexChild]
+		currentNode = currentNode.Children[indexChild]
 
 	}
 
@@ -85,20 +85,20 @@ func (bTree *BTree) Search(key string) *Node {
 func (bTree *BTree) sortKeys(record *Record.Record, position *Node) {
 	index := -1
 	for i := 0; i < position.n; i++ {
-		if record.GetKey() < position.keys[i].GetKey() {
+		if record.GetKey() < position.Keys[i].GetKey() {
 			index = i
 			break
 		}
 	}
 	if index == -1 {
-		position.keys[position.n] = record
+		position.Keys[position.n] = record
 		position.n = position.n + 1
 	} else {
 
 		for index != position.n+1 {
-			temp := position.keys[index]
-			position.keys[index] = record
-			// record = position.keys[index+1]
+			temp := position.Keys[index]
+			position.Keys[index] = record
+			// record = position.Keys[index+1]
 			record = temp
 			index++
 		}
@@ -112,61 +112,61 @@ func (bTree *BTree) AddElement(record *Record.Record) *Node {
 
 	//tree is empty
 	if position == nil {
-		rootNode := createNode(bTree.T)
-		rootNode.keys[0] = record
-		rootNode.n = 1
-		bTree.root = rootNode
-		return rootNode
+		RootNode := createNode(bTree.T)
+		RootNode.Keys[0] = record
+		RootNode.n = 1
+		bTree.Root = RootNode
+		return RootNode
 	}
 	//record already in tree
 	for i := 0; i < position.n; i++ {
-		if position.keys[i].GetKey() == record.GetKey() {
+		if position.Keys[i].GetKey() == record.GetKey() {
 			return position
 		}
 	}
 
-	//tree just have a root node
-	if position == bTree.root {
-		//root is full
+	//tree just have a Root node
+	if position == bTree.Root {
+		//Root is full
 		if position.T-1 == position.n {
 			//adding record and sorting with that extra record
-			position.keys = append(position.keys, nil)
+			position.Keys = append(position.Keys, nil)
 			bTree.sortKeys(record, position)
 
 			//newRoot
 			newRootNode := createNode(bTree.T)
-			newRootNode.keys[0] = position.keys[(position.n-1)/2]
+			newRootNode.Keys[0] = position.Keys[(position.n-1)/2]
 			newRootNode.isLeaf = false
 			newRootNode.n = 1
-			position.keys[(position.n-1)/2] = nil
+			position.Keys[(position.n-1)/2] = nil
 
 			//leftChild
 			leftChildNode := createNode(bTree.T)
 			for i := 0; i < position.n; i++ {
-				if position.keys[i] == nil {
+				if position.Keys[i] == nil {
 					break
 				}
-				leftChildNode.keys[i] = position.keys[i]
+				leftChildNode.Keys[i] = position.Keys[i]
 				leftChildNode.n += 1
 			}
 			//RightChild
 			rightChildNode := createNode(bTree.T)
 			index := 0
 			for i := (position.n-1)/2 + 1; i < position.n; i++ {
-				rightChildNode.keys[index] = position.keys[i]
+				rightChildNode.Keys[index] = position.Keys[i]
 				rightChildNode.n += 1
 				index++
 			}
 
-			newRootNode.children[0] = leftChildNode
-			newRootNode.children[1] = rightChildNode
+			newRootNode.Children[0] = leftChildNode
+			newRootNode.Children[1] = rightChildNode
 			leftChildNode.parent = newRootNode
 			rightChildNode.parent = newRootNode
 
-			bTree.root = newRootNode
+			bTree.Root = newRootNode
 
 		} else {
-			//root is not full
+			//Root is not full
 			bTree.sortKeys(record, position)
 
 		}
@@ -174,34 +174,36 @@ func (bTree *BTree) AddElement(record *Record.Record) *Node {
 		//overflow
 		if position.T-1 == position.n {
 
-			position.keys = append(position.keys, nil)
+			position.Keys = append(position.Keys, nil)
 			bTree.sortKeys(record, position)
 
 			parentNode := position.parent
-			overflowRecord := position.keys[(position.n-1)/2]
+			overflowRecord := position.Keys[(position.n-1)/2]
 
-			position.keys[(position.n-1)/2] = nil
-			//leftChild
-			leftChildNode := createNode(bTree.T)
-			for i := 0; i < position.n; i++ {
-				if position.keys[i] == nil {
-					break
-				}
-				leftChildNode.keys[i] = position.keys[i]
-			}
-			//RightChild
-			rightChildNode := createNode(bTree.T)
-			index := 0
-			for i := (position.n-1)/2 + 1; i < position.n; i++ {
-				rightChildNode.keys[index] = position.keys[i]
-				index++
-			}
+			position.Keys[(position.n-1)/2] = nil
 
 			//promotion
 			if parentNode.T-1 == parentNode.n+1 {
 
 			} else {
 				//parent go up
+
+				//leftChild
+				leftChildNode := createNode(bTree.T)
+				for i := 0; i < position.n; i++ {
+					if position.Keys[i] == nil {
+						break
+					}
+					leftChildNode.Keys[i] = position.Keys[i]
+				}
+				//RightChild
+				rightChildNode := createNode(bTree.T)
+				index := 0
+				for i := (position.n-1)/2 + 1; i < position.n; i++ {
+					rightChildNode.Keys[index] = position.Keys[i]
+					index++
+				}
+
 				bTree.sortKeys(overflowRecord, parentNode)
 				newChildren := make([]*Node, bTree.T)
 				for i := 0; i < bTree.T; i++ {
@@ -211,12 +213,12 @@ func (bTree *BTree) AddElement(record *Record.Record) *Node {
 				k := 0
 				// is_append := false
 
-				for i < len(parentNode.children) {
-					if parentNode.children[i] == nil {
+				for i < len(parentNode.Children) {
+					if parentNode.Children[i] == nil {
 						break
 					}
-					if parentNode.children[i] != position {
-						newChildren[k] = parentNode.children[i]
+					if parentNode.Children[i] != position {
+						newChildren[k] = parentNode.Children[i]
 						k++
 						i++
 					} else {
@@ -227,8 +229,8 @@ func (bTree *BTree) AddElement(record *Record.Record) *Node {
 						i++
 					}
 				}
-				fmt.Println(newChildren)
-				parentNode.children = newChildren
+				// fmt.Println(newChildren)
+				parentNode.Children = newChildren
 
 			}
 
@@ -240,49 +242,23 @@ func (bTree *BTree) AddElement(record *Record.Record) *Node {
 	return position
 }
 
-// func (bTree *BTree) print(record *record.Record) *Node {
-
-// }
-
-func main() {
-	bTree := createBTree(4)
-	record1 := Record.NewRecordKeyValue("123", []byte{100, 20}, 0)
-	bTree.AddElement(record1)
-	// keys := bTree.AddElement(record1).keys
-	// for i := 0; i < 3; i++ {
-	// 	fmt.Println(keys[i])
-	// }
-	record2 := Record.NewRecordKeyValue("23", []byte{100, 20}, 0)
-	bTree.AddElement(record2)
-	// keys := bTree.AddElement(record2).keys
-
-	record3 := Record.NewRecordKeyValue("456", []byte{100, 20}, 0)
-	bTree.AddElement(record3)
-	// keys := bTree.AddElement(record3).keys
-	// for i := 0; i < 3; i++ {
-	// 	fmt.Println(keys[i])
-	// }
-
-	record4 := Record.NewRecordKeyValue("678", []byte{100, 20}, 0)
-	bTree.AddElement(record4)
-
-	// keys = bTree.AddElement(record4).keys
-
-	// for i := 0; i < 3; i++ {
-	// 	fmt.Println(keys[i])
-	// }
-
-	record5 := Record.NewRecordKeyValue("534", []byte{100, 20}, 0)
-	bTree.AddElement(record5)
-	// keys = bTree.AddElement(record5).parent.keys
-	// fmt.Println(bTree.AddElement(record5).parent.children[0].keys)
-	// for i := 0; i < 3; i++ {
-	// 	fmt.Println(keys[i])
-	// }
-
-	record6 := Record.NewRecordKeyValue("537", []byte{100, 20}, 0)
-	bTree.AddElement(record6)
-
-	fmt.Println(bTree.root.children[2].keys)
+func (bTree *BTree) Print(root *Node) {
+	if root != nil {
+		for _, element := range root.Keys {
+			if element != nil {
+				fmt.Print(element.GetKey() + "  ")
+			}
+		}
+		fmt.Println("")
+		if root.Children[0] != nil {
+			for _, element := range root.Children {
+				bTree.Print(element)
+			}
+		}
+	}
 
 }
+
+// func main() {
+
+// }
