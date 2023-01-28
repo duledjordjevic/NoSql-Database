e3a7ac84269964ad81cf4a007de799fd262baf5e
duledjordjevic <djordjevic.sv1.2021@uns.ac.rs>
2023-01-28 16:25:21 +0100

    Upload HLL and SkipList

--- Structures/HLL/HLL.go
+++ Structures/HLL/HLL.go
@@ -1,1 +1,104 @@
 package HLL
+
+import (
+	"fmt"
+	"hash/fnv"
+	"math"
+	"math/bits"
+)
+
+const (
+	HLL_MIN_PRECISION = 4
+	HLL_MAX_PRECISION = 16
+)
+
+type HLL struct {
+	m         uint64
+	p         uint8
+	registers []uint8
+}
+
+// procena kardinalnosti
+func (hll *HLL) Estimate() float64 {
+	sum := 0.0
+	for _, val := range hll.registers {
+		sum += math.Pow(math.Pow(2.0, float64(val)), -1)
+	}
+
+	alpha := 0.7213 / (1.0 + 1.079/float64(hll.m))
+	estimation := alpha * math.Pow(float64(hll.m), 2.0) / sum
+	emptyRegs := hll.emptyCount()
+
+	if estimation <= 2.5*float64(hll.m) { // do small range correction
+		if emptyRegs > 0 {
+			estimation = float64(hll.m) * math.Log(float64(hll.m)/float64(emptyRegs))
+		}
+	} else if estimation > 1/30.0*math.Pow(2.0, 32.0) { // do large range correction
+		estimation = -math.Pow(2.0, 32.0) * math.Log(1.0-estimation/math.Pow(2.0, 32.0))
+	}
+	return estimation
+}
+
+func (hll *HLL) emptyCount() int {
+	sum := 0
+	for _, val := range hll.registers {
+		if val == 0 {
+			sum++
+		}
+	}
+	return sum
+}
+
+func createHLL(p uint8) *HLL {
+	if p > HLL_MAX_PRECISION || p < HLL_MIN_PRECISION {
+		panic("Doslo je do greske. Broj p mora biti u opsegu : [4,16]")
+	}
+	hll := HLL{}
+	hll.p = p
+	hll.m = uint64(math.Pow(2, float64(hll.p)))
+	hll.registers = make([]uint8, hll.m, hll.m)
+
+	return &hll
+}
+
+// func (hll *HLL) createHashFunction() {
+
+// }
+
+func (hll *HLL) addElement(element string) {
+
+	h := fnv.New32()
+	_, err := h.Write([]byte(element))
+	if err != nil {
+		panic(err)
+	}
+
+	bytes := h.Sum32()
+
+	index := bytes >> (32 - hll.p)
+	endZeros := bits.TrailingZeros32(bytes)
+
+	if hll.registers[index] < uint8(endZeros) {
+		hll.registers[index] = uint8(endZeros)
+	}
+}
+
+func main() {
+
+	hll := createHLL(6)
+
+	hll.addElement("string")
+	hll.addElement("string")
+	hll.addElement("string")
+	hll.addElement("string")
+	hll.addElement("string")
+	hll.addElement("string")
+	hll.addElement("string")
+	hll.addElement("text")
+	hll.addElement("text1")
+	// hll.addElement("1")
+	// hll.addElement("asdjoiasdj")
+
+	fmt.Println(hll.Estimate())
+	fmt.Println(hll.registers)
+}
--- Structures/Skiplist/skiplist.go
+++ Structures/Skiplist/skiplist.go
@@ -1,1 +1,238 @@
 package skiplist
+
+import (
+	"NAiSP/Structures/Record"
+	"math/rand"
+	"time"
+)
+
+const (
+	NEG_INFINITY = ""
+	POS_INFINITY = "■"
+)
+
+type Structure interface {
+	Search(key string) *Node
+	AddElement(record Record.Record) *Node
+}
+
+type Node struct {
+	above *Node
+	below *Node
+	next  *Node
+	prev  *Node
+	key   string
+	Value *Record.Record
+}
+
+func createNode(value *Record.Record) *Node {
+	node := &Node{
+		key:   value.GetKey(),
+		Value: value,
+		above: nil,
+		below: nil,
+		next:  nil,
+		prev:  nil,
+	}
+	return node
+}
+
+type SkipList struct {
+	head *Node
+	tail *Node
+
+	height uint
+}
+
+func createSkipList() *SkipList {
+	skipList := &SkipList{}
+	negRecord := Record.NewRecordKeyValue(NEG_INFINITY, []byte{1, 2}, byte(0))
+	posRecord := Record.NewRecordKeyValue(POS_INFINITY, []byte{1, 2}, byte(0))
+	skipList.head = createNode(negRecord)
+	skipList.tail = createNode(posRecord)
+	skipList.head.next = skipList.tail
+	skipList.tail.prev = skipList.head
+
+	skipList.height = 0
+	return skipList
+}
+
+func (skipList *SkipList) Search(key string) *Node {
+	n := skipList.head
+
+	for n.below != nil {
+		// fmt.Println("hello")
+		n = n.below
+
+		for key >= n.next.key {
+			n = n.next
+		}
+	}
+
+	return n
+
+}
+
+func (skipList *SkipList) AddElement(value *Record.Record) *Node {
+	position := skipList.Search(value.GetKey())
+
+	var q *Node
+
+	var level int = -1
+	var numberOfHeads int = -1
+
+	if value.GetKey() == position.key {
+		return position
+	}
+
+	for {
+		numberOfHeads++
+		level++
+
+		skipList.canIncreaseLevel(level)
+
+		q = position
+
+		for position.above == nil {
+			position = position.prev
+		}
+
+		position = position.above
+		// fmt.Println(&position)
+		q = skipList.insertAfterAbove(position, q, value)
+
+		if rand.Seed(time.Now().UnixNano()); rand.Intn(2) == 0 {
+			break
+		}
+	}
+	return q
+}
+
+func (skipList *SkipList) canIncreaseLevel(level int) {
+	if level >= int(skipList.height) {
+		skipList.height += 1
+		skipList.addEmptyLevel()
+	}
+}
+
+func (skipList *SkipList) addEmptyLevel() {
+	negRecord := Record.NewRecordKeyValue(NEG_INFINITY, []byte{1, 2}, byte(0))
+	posRecord := Record.NewRecordKeyValue(POS_INFINITY, []byte{1, 2}, byte(0))
+	newHeadNode := createNode(negRecord)
+	newTailNode := createNode(posRecord)
+
+	newHeadNode.next = newTailNode
+	newHeadNode.below = skipList.head
+	newTailNode.prev = newHeadNode
+	newTailNode.below = skipList.tail
+
+	skipList.head.above = newHeadNode
+	skipList.tail.above = newTailNode
+
+	// fmt.Println(&skipList.head)
+	skipList.head = newHeadNode
+	skipList.tail = newTailNode
+
+}
+
+func (skipList *SkipList) insertAfterAbove(position *Node, q *Node, value *Record.Record) *Node {
+
+	newNode := createNode(value)
+	// fmt.Println(position.below.key)
+
+	nodeBeforeNewNode := position.below.below
+
+	skipList.setBeforeAndAfterReferences(q, newNode)
+	skipList.setAboveAndBelowReferences(position, value, newNode, nodeBeforeNewNode)
+
+	return newNode
+}
+
+func (skipList *SkipList) setBeforeAndAfterReferences(q *Node, newNode *Node) {
+	newNode.next = q.next
+	newNode.prev = q
+	q.next.prev = newNode
+	q.next = newNode
+
+}
+
+func (skipList *SkipList) setAboveAndBelowReferences(position *Node, value *Record.Record, newNode *Node, nodeBeforeNewNode *Node) {
+	if nodeBeforeNewNode != nil {
+		for {
+			if nodeBeforeNewNode.next.key != value.GetKey() {
+				nodeBeforeNewNode = nodeBeforeNewNode.next
+			} else {
+				break
+			}
+		}
+		newNode.below = nodeBeforeNewNode.next
+
+		nodeBeforeNewNode.next.above = newNode
+	}
+
+	if position != nil {
+		if position.next.key == value.GetKey() {
+			newNode.above = position.next
+		}
+	}
+
+}
+
+// func (skipList *SkipList) removeElement(value) bool {
+// 	nodeToBeRemoved := skipList.Search(key)
+
+// 	if nodeToBeRemoved.key != key {
+// 		return false
+// 	}
+
+// 	skipList.removeReferencesToNode(nodeToBeRemoved)
+
+// 	for nodeToBeRemoved != nil {
+// 		skipList.removeReferencesToNode(nodeToBeRemoved)
+
+// 		if nodeToBeRemoved.above != nil {
+// 			nodeToBeRemoved = nodeToBeRemoved.above
+// 		} else {
+// 			break
+// 		}
+
+// 	}
+// 	return true
+// }
+
+// func (skipList *SkipList) removeReferencesToNode(nodeToBeRemoved *Node) {
+
+// 	afterNodeToBeRemoved := nodeToBeRemoved.next
+// 	beforeNodeToBeRemoved := nodeToBeRemoved.prev
+
+// 	beforeNodeToBeRemoved.next = afterNodeToBeRemoved
+// 	afterNodeToBeRemoved.prev = beforeNodeToBeRemoved
+
+// }
+
+func main() {
+	// rand.Seed(time.Now().UnixNano())
+	// fmt.Println(rand.Intn(2))
+
+	skipList := createSkipList()
+
+	// fmt.Println(skipList.head.key)
+	negRecord := Record.NewRecordKeyValue("10", []byte{1, 2}, byte(0))
+	// print(skipList.AddElement(negRecord))
+	// skipList.AddElement(negRecord)
+	// negRecord = record.NewRecordKeyValue(NEG_INFINITY, []byte{1, 2}, byte(0))
+	skipList.AddElement(negRecord)
+	negRecord = Record.NewRecordKeyValue("30", []byte{1, 2}, byte(0))
+	skipList.AddElement(negRecord)
+	negRecord = Record.NewRecordKeyValue("Dusan", []byte{1, 2}, byte(0))
+	skipList.AddElement(negRecord)
+	negRecord = Record.NewRecordKeyValue("Rade", []byte{1, 2}, byte(0))
+	skipList.AddElement(negRecord)
+	// skipList.AddElement(negRecord)
+	print(skipList.Search("10").key)
+	print(skipList.Search("30").key)
+	print(skipList.Search("Rade").key)
+	print(skipList.Search("Trajce").key)
+	// fmt.Println(skipList.removeElement(10))
+
+}
