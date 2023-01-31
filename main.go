e2ed5e459f3a2da8e92626f08f6f0718f3f58f06
radepejanvic <pejanovic.sv10.2021@uns.ac.rs>
2023-01-31 23:05:49 +0100

    Write Path
    Co-Authored-By: trajcex <105116816+trajcex@users.noreply.github.com>
    Co-Authored-By: duledjordjevic <104204960+duledjordjevic@users.noreply.github.com>
    Co-Authored-By: pejinovics <75089955+pejinovics@users.noreply.github.com>
    Co-Authored-By: maricnikola <105132124+maricnikola@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,29 +1,40 @@
 package main
 
 import (
+	bloomfilter "NAiSP/Structures/Bloomfilter"
+	memtable "NAiSP/Structures/Memtable"
+	record "NAiSP/Structures/Record"
+	wal "NAiSP/Structures/WAL"
 	writepath "NAiSP/Structures/WritePath"
-	"fmt"
 )
 
 func main() {
-	// test1Record := record.NewRecordKeyValue("a", []byte{123, 31}, byte(0))
-	// test2Record := record.NewRecordKeyValue("b", []byte{123, 31}, byte(0))
-	// test3Record := record.NewRecordKeyValue("c", []byte{123, 31}, byte(0))
-	// test4Record := record.NewRecordKeyValue("e", []byte{123, 31}, byte(0))
-	// test5Record := record.NewRecordKeyValue("f", []byte{123, 31}, byte(0))
-	// test6Record := record.NewRecordKeyValue("g", []byte{123, 31}, byte(0))
-	// test7Record := record.NewRecordKeyValue("h", []byte{123, 31}, byte(0))
-	// test8Record := record.NewRecordKeyValue("i", []byte{123, 31}, byte(0))
-	// test9Record := record.NewRecordKeyValue("j", []byte{123, 31}, byte(0))
-	// test10Record := record.NewRecordKeyValue("k", []byte{123, 31}, byte(0))
-	// test11Record := record.NewRecordKeyValue("l", []byte{123, 31}, byte(0))
-	// test12Record := record.NewRecordKeyValue("m", []byte{123, 31}, byte(0))
-	// test13Record := record.NewRecordKeyValue("n", []byte{123, 31}, byte(0))
-	// test14Record := record.NewRecordKeyValue("o", []byte{123, 31}, byte(0))
-	// test15Record := record.NewRecordKeyValue("p", []byte{123, 31}, byte(0))
-	// test16Record := record.NewRecordKeyValue("q", []byte{123, 31}, byte(0))
-	// test17Record := record.NewRecordKeyValue("s", []byte{123, 31}, byte(0))
-	// lista := []record.Record{*test1Record, *test2Record, *test3Record, *test4Record, *test5Record, *test6Record, *test7Record, *test8Record, *test9Record, *test10Record, *test11Record, *test12Record, *test13Record, *test14Record, *test15Record, *test16Record, *test17Record}
+	test1Record := record.NewRecordKeyValue("a", []byte{123, 31}, byte(0))
+	test2Record := record.NewRecordKeyValue("b", []byte{123, 31}, byte(0))
+	test3Record := record.NewRecordKeyValue("c", []byte{123, 31}, byte(0))
+	test4Record := record.NewRecordKeyValue("e", []byte{123, 31}, byte(0))
+	test5Record := record.NewRecordKeyValue("f", []byte{123, 31}, byte(0))
+	test6Record := record.NewRecordKeyValue("g", []byte{123, 31}, byte(0))
+	test7Record := record.NewRecordKeyValue("h", []byte{123, 31}, byte(0))
+	test8Record := record.NewRecordKeyValue("i", []byte{123, 31}, byte(0))
+	test9Record := record.NewRecordKeyValue("j", []byte{123, 31}, byte(0))
+	test10Record := record.NewRecordKeyValue("k", []byte{123, 31}, byte(0))
+	test11Record := record.NewRecordKeyValue("l", []byte{123, 31}, byte(0))
+	test12Record := record.NewRecordKeyValue("m", []byte{123, 31}, byte(0))
+	test13Record := record.NewRecordKeyValue("n", []byte{123, 31}, byte(0))
+	test14Record := record.NewRecordKeyValue("o", []byte{123, 31}, byte(0))
+	test15Record := record.NewRecordKeyValue("p", []byte{123, 31}, byte(0))
+	test16Record := record.NewRecordKeyValue("q", []byte{123, 31}, byte(0))
+	test17Record := record.NewRecordKeyValue("s", []byte{123, 31}, byte(0))
+	lista := []record.Record{*test1Record, *test2Record, *test3Record, *test4Record, *test5Record, *test6Record, *test7Record, *test8Record, *test9Record, *test10Record, *test11Record, *test12Record, *test13Record, *test14Record, *test15Record, *test16Record, *test17Record}
+
+	BF := bloomfilter.NewBLoomFilter(100, 0.01)
+	WAL := wal.NewWal()
+	MemTable := memtable.CreateMemtable(10, 0.8, "btree")
+	wp := writepath.WritePath{Wal: WAL, MemTable: MemTable, BloomFilter: BF}
+	for _, record := range lista {
+		wp.Write(&record)
+	}
 
 	// tabela := sstable.NewSStable("data.bin", "index.bin", "summary.bin", "bloom.gob", "metadata.txt", "TOC.txt")
 	// tabela.FormSStable(lista)
@@ -43,5 +54,6 @@ func main() {
 	// tocTabela := sstable.NewSStableFromTOC("TOC.txt")
 	// fmt.Println(tocTabela.DataTablePath, tocTabela.BloomFilterPath)
 
-	fmt.Println(writepath.GenerateFileName("Leveled"))
+	// fmt.Println(writepath.GenerateFileName("Leveled"))
+
 }
