49572b715ca69e91299f164f53a7a1fe925d3359
radepejanvic <pejanovic.sv10.2021@uns.ac.rs>
2023-02-01 19:43:31 +0100

    LSM correction
    Co-Authored-By: duledjordjevic <104204960+duledjordjevic@users.noreply.github.com>
    Co-Authored-By: maricnikola <105132124+maricnikola@users.noreply.github.com>
    Co-Authored-By: pejinovics <75089955+pejinovics@users.noreply.github.com>
    Co-Authored-By: trajcex <105116816+trajcex@users.noreply.github.com>

--- Structures/LSM/lsm.go
+++ Structures/LSM/lsm.go
@@ -1,9 +1,33 @@
 package lsm
 
-func Leveled() {
+import (
+	configreader "NAiSP/Structures/ConfigReader"
+	sstable "NAiSP/Structures/Sstable"
+	"fmt"
+	"os"
+)
 
+type LSM struct {
+	Config  *configreader.ConfigReader
+	ssTable *sstable.SStable
 }
 
-func SizeTiered() {
+// Open file depending on DataFileStructure
+func (lsm *LSM) OpenData(fileName string) *os.File {
 
+	// Open file
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error, ", err)
+	}
+
+	// Seek to start
+	file.Seek(0, 0)
+
+	if lsm.Config.DataFileStructure == "Single" {
+		// Read header
+		// Seek on data part
+	}
+
+	return file
 }
