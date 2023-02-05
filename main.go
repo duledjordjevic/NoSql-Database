6b58e05f1a67ad8541e0a6bcd5e3be854a9a6428
duledjordjevic <djordjevic.sv1.2021@uns.ac.rs>
2023-02-05 23:56:21 +0100

    kraj
    Co-Authored-By: maricnikola <105132124+maricnikola@users.noreply.github.com>
    Co-Authored-By: pejinovics <75089955+pejinovics@users.noreply.github.com>
    Co-Authored-By: radepejanvic <104909226+radepejanvic@users.noreply.github.com>
    Co-Authored-By: trajcex <105116816+trajcex@users.noreply.github.com>

--- main.go
+++ main.go
@@ -5,13 +5,6 @@ import (
 	menu "NAiSP/Menu"
 )
 
-// bloomfilter "NAiSP/Structures/Bloomfilter"
-// memtable "NAiSP/Structures/Memtable"
-// record "NAiSP/Structures/Record"
-// wal "NAiSP/Structures/WAL"
-// writepath "NAiSP/Structures/WritePath"
-
-// tester "NAiSP/Test"
 
 func main() {
 	app := application.CreateApp()
